56788850a8f1f237348c54ca069df75d01db3a00
Sanjog Thapa <sanzog03@gmail.com>
2024-07-26 16:18:06 -0500

    add react resizeable panels for chart and map. also added styled div

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Box from '@mui/material/Box';
+import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
 
 import { FCXViewer } from '../cesiumViewer';
 import { ConcentrationChart } from '../chart';
 import { Title } from '../title';
+import styled from 'styled-components';
 
 function Home() {
   return (
@@ -12,8 +14,18 @@ function Home() {
       flexDirection={"column"}
       className="fullSize">
         <Title/>
-        <FCXViewer/>
-        <ConcentrationChart/>
+        <PanelGroup direction='vertical' className='panel-wrapper'>
+          <Panel
+            maxSize={75}
+            className='panel'
+          >
+            <FCXViewer/>
+          </Panel>
+          <PanelResizeHandle className='resize-handle' />
+          <Panel maxSize={75} minSize={40} defaultSize={40} className='panel panel-timeline'>
+            <ConcentrationChart/>
+          </Panel>
+        </PanelGroup>
     </Box>
   );
 }
